942e85fb1bd80e04dc6d9ba46af60f6c61dd0c3d
LloydGhettoWolf <lloyd.crawley@gmail.com>
2020-05-05 10:14:30 +0200

    halfway through adding testing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,68 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga;
+mod serial;
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello {}", 42);
-    panic!("some panic!");
+    
+    #[cfg(test)]
+    test_main();
+
     loop{}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[Failed]\n");
+    serial_println!("Error: {}\n", _info);
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests{
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("Trivial assertion...");
+    assert_eq!(0,1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
