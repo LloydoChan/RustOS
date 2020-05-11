39debcd6089239714d3ba8fa33ceba0761ac7274
LloydGhettoWolf <lloyd.crawley@gmail.com>
2020-05-11 09:11:00 +0200

    added keyboard support

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub fn test_panic_handler(_info: &PanicInfo) -> ! {
     serial_println!("[Failed]\n");
     serial_println!("Error: {}\n", _info);
     exit_qemu(QemuExitCode::Failed);
-    loop{}
+    hlt_loop()
 }
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
@@ -33,7 +33,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop{}
+    hlt_loop()
 }
 
 #[cfg(test)]
@@ -62,4 +62,12 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop{
+        x86_64::instructions::hlt();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,18 @@ pub extern "C" fn _start() -> ! {
     println!("hello");
     RustOS::init();
 
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    }
-
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-    loop{}
+    RustOS::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
-    loop {}
+    RustOS::hlt_loop();
 }
 
 #[cfg(test)]
